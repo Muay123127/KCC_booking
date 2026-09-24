1d86b17f3e15c06e6555c83554f862d2217baa11
Muay123127 <p2807418@gmail.com>
2026-09-24 11:25:56 +0700

    fix viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -13,6 +14,7 @@ export default defineConfig(({ mode }) => {
   plugins: [
     vue(),
     tailwindcss(),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
